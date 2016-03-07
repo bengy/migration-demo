4bdd334c487cc7724ac2a02e6f9060564f1da0bd
Tobias Schiller <toschill@htwg-konstanz.de>
2016-03-07 22:18:52 +0100

    removed build from .gitignore

--- backend/conf/evolutions/default/1.sql
+++ backend/conf/evolutions/default/1.sql
@@ -3,14 +3,14 @@
 
 # --- !Ups
 
-create table modell_user (
-  muser_id                  bigint not null,
+create table client (
+  client_id                 bigint not null,
   name                      varchar(255),
   lang                      varchar(255),
-  constraint pk_modell_user primary key (muser_id))
+  constraint pk_client primary key (client_id))
 ;
 
-create sequence modell_user_seq;
+create sequence client_seq;
 
 
 
@@ -19,9 +19,9 @@ create sequence modell_user_seq;
 
 SET REFERENTIAL_INTEGRITY FALSE;
 
-drop table if exists modell_user;
+drop table if exists client;
 
 SET REFERENTIAL_INTEGRITY TRUE;
 
-drop sequence if exists modell_user_seq;
+drop sequence if exists client_seq;
 
